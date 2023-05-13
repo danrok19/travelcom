8391d443e410ace67452e821c2370e2f3b86b5fc
Kobe <danrok19@wp.pl>
2023-05-13 19:09:17 +0200

    start of home with searching div

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './home.css';
+import video from '../../Assets/ocean-65560.mp4';
+import { GrLocation } from 'react-icons/gr';
+
+const Home = () => {
+  return (
+    <section className='home'>
+        <div className="overlay" />
+        <video src={video} muted autoPlay loop type="video/mp4"/>
+        <div className='homeContent container'>
+            <div className="textDiv">
+                <span className="smallText">
+                    Our Packages
+                </span>
+                <h1 className="homeTitle">
+                    Search your holiday
+                </h1>
+            </div>
+            <div className="cardDiv grid">
+                <div className="destinationInput">
+                    <label htmlFor='city'>Search your destination:</label>
+                    <div className='input flex'>
+                        <input type="text" placeholder='Enter name here...'/>
+                        <GrLocation className='icon'/>
+                    </div>
+                </div>
+                <div className="dateInput">
+                    <label htmlFor='city'>Select date:</label>
+                    <div className='input flex'>
+                        <input type="date" />
+                    </div>
+                </div>
+                <div className="priceInput">
+                    <div className="label_total flex">
+                        <label hrmlFor="price">Max price:</label>
+                        <h3 className="total">$5000</h3>
+                    </div>
+                    <div className="input flex">
+                        <input type="range" max="5000" min="1000"></input>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home
