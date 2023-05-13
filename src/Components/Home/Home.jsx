08f7b96377dee16dab63fd8d179bc323d3ddf0c4
Kobe <danrok19@wp.pl>
2023-05-13 22:05:35 +0200

    finish home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import './home.css';
 import video from '../../Assets/ocean-65560.mp4';
 import { GrLocation } from 'react-icons/gr';
+import { HiFilter } from 'react-icons/hi';
+import { FiFacebook } from 'react-icons/fi';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { FaTripadvisor } from 'react-icons/fa';
+import { BsListTask } from 'react-icons/bs';
+import { TbApps } from 'react-icons/tb';
 
 const Home = () => {
   return (
@@ -39,6 +45,23 @@ const Home = () => {
                     <div className="input flex">
                         <input type="range" max="5000" min="1000"></input>
                     </div>
+                </div>
+                <div className='searchOptions'>
+                    <HiFilter className='icon'/>
+                    <span>More Filters</span>
+
+                </div>
+            </div>
+
+            <div className="homeFooterIcons flex">
+                <div className="rightIcons">
+                    <FiFacebook className='icon'/>
+                    <AiOutlineInstagram className='icon'/>
+                    <FaTripadvisor className='icon'/>
+                </div>
+                <div className="leftIcons">
+                    <BsListTask className='icon'/>
+                    <TbApps className='icon'/>
                 </div>
             </div>
         </div>
